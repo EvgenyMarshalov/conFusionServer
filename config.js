59eb2826eb7a5809b84e5b410090c238fb20e4c7
Evgeny Marshalov <evgeny.marshalov@gmail.com>
2020-06-24 19:24:42 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '2623004377961821',
+        clientSecret: 'f15d556628d54ee7c79736726eee90ab'
+    }
+
 }
